661b4be64596f42bbeb5dbb9ea043d2a4f0c1c53
Gopikrishnan Sasikumar <gopikrishnans1996@gmail.com>
2017-08-28 20:57:33 +0530

    floral carpet using python turtle

---
+++ floral_carpet/bash/second.py
@@ -0,0 +1,27 @@
+from turtle import *
+speed(0)
+pencolor('yellow')
+bgcolor('black')
+x=0
+up()
+rt(45)
+fd(90)
+rt(135)
+down()
+while x<120:
+    fd(200)
+    rt(61)
+    fd(200)
+    rt(61)
+    fd(200)
+    rt(61)
+    fd(200)
+    rt(61)
+    fd(200)
+    rt(61)
+    fd(200)
+    rt(61)
+
+    rt(11.1111)
+    x=x+1
+exitonclick()
